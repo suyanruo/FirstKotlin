1cd64ffad7a5ffe5618307c23e6f405e54fa79b8
suyanruo <dimghost1@gmail.com>
2021-08-04 15:38:17 +0800

    +)crime界面设计

---
+++ criminalintent/src/main/java/com/example/criminalintent/activity/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.criminalintent.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.criminalintent.R
+import com.example.criminalintent.fragment.CrimeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val curFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (curFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+}
--- criminalintent/src/main/java/com/example/criminalintent/fragment/CrimeFragment.kt
+++ criminalintent/src/main/java/com/example/criminalintent/fragment/CrimeFragment.kt
@@ -1,20 +1,63 @@
 package com.example.criminalintent.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.example.criminalintent.R
+import com.example.criminalintent.model.Crime
 
 class CrimeFragment : Fragment() {
+    private lateinit var etTitle: EditText
+    private lateinit var btnDate: Button
+    private lateinit var cbSolved: CheckBox
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crime, container, false)
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        etTitle = view.findViewById(R.id.et_crime_title)
+        btnDate = view.findViewById(R.id.btn_crime_date)
+        cbSolved = view.findViewById(R.id.cb_solved)
+
+        btnDate.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // 因为设备配置放生变化时会重新执行声明周期函数，而数据存储在viewmodel中，如果在onCreateView中设置监听器，视图恢复后在数据重置时会触发监听器
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        etTitle.addTextChangedListener(textWatcher)
+
+        cbSolved.apply {
+            setOnCheckedChangeListener { _, isChecked -> crime.isSolved = isChecked }
+        }
     }
 
     companion object {
